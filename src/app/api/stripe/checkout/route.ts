facff069f7afeba63507a8fd6ac1add50646d54a
Mup <85489543+muperdev@users.noreply.github.com>
2025-02-08 09:06:54 +0400

    fix: build issue with stripe version

--- src/app/api/stripe/checkout/route.ts
+++ src/app/api/stripe/checkout/route.ts
@@ -5,7 +5,7 @@ import { updateUserStripeId } from '@/lib/stripe'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-01-27.acacia',
 })
 
 // Ensure URL has proper scheme
