6f8e513e9337823bc530d161d7c3e3d1b0e3221b
Mup <85489543+muperdev@users.noreply.github.com>
2025-07-09 11:41:41 +0400

    feat: pixel (x) tracker

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { Black_Han_Sans, Open_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import { CSPostHogProvider } from './provider'
 import { ToastProvider } from '@/components/providers/toast-provider'
+import Script from 'next/script'
 
 const blackHanSans = Black_Han_Sans({
   subsets: ['latin'],
@@ -32,6 +33,20 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           {children}
           <Analytics />
           <ToastProvider />
+
+          {/* Twitter conversion tracking */}
+          <Script
+            id="twitter-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+                },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+                twq('config','q4kzc');
+              `,
+            }}
+          />
         </body>
       </CSPostHogProvider>
     </html>
