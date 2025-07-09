67248e9462f0cd4ed68e57c2abeb4551da7f8d2f
Mup <85489543+muperdev@users.noreply.github.com>
2025-07-09 11:58:59 +0400

    fix: move twitter pixel tag into head tag

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -26,6 +26,21 @@ export const metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Twitter conversion tracking */}
+        <Script
+          id="twitter-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              twq('config','q4kzc');
+            `,
+          }}
+        />
+      </head>
       <CSPostHogProvider>
         <body
           className={`${blackHanSans.variable} ${openSans.variable} font-black-han-sans bg-black text-neutral-400`}
@@ -33,20 +48,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           {children}
           <Analytics />
           <ToastProvider />
-
-          {/* Twitter conversion tracking */}
-          <Script
-            id="twitter-pixel"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-                },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-                twq('config','q4kzc');
-              `,
-            }}
-          />
         </body>
       </CSPostHogProvider>
     </html>
